a604268a162d280bf1a6b70d4bbdea799117f0c4
davidfine <dfine@bittorrent.com>
2016-02-11 19:50:09 -0800

    Add comments

--- geiger_counter/geiger_counter.ino
+++ geiger_counter/geiger_counter.ino
@@ -1,9 +1,12 @@
+// Goes into a geiger counter prop, beeps and flashes
+// when an object is between ~6-18 inches from sensor.
+// Uses a control voltage from an LV‑MaxSonar‑EZ1 
 
 #define ledPin 8
 #define sensorPin A0
-#define buzzerPin 11
-#define tempo 20
-#define pitch 1600
+#define buzzerPin 11  // piezzo buzzer
+#define tempo 20 // affects the beep/flash 
+#define pitch 1600  // audible freq of beep
 
 int range = 6;
 int x = 0;
@@ -24,7 +27,7 @@ int readSensor() {
 
 void loop() {
   if (millis() - time_of_last_beep > 10000){
-    sleep_time = 1500;}   // power saving
+    sleep_time = 1500;}   // power saving mode
   else { 
     sleep_time = 50;}  //active state
   delay(sleep_time); // Save power
